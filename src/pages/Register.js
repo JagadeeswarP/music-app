805c997527a31ad704a7d85746dd11ca63233339
JagadeeswarP <jagadeeswarpattupogula@gmail.com>
2024-07-12 19:32:00 +0530

    added popup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,7 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import LoginSuccessPopup from './LoginSuccessPopup';
  
 
 const Register = () => {
